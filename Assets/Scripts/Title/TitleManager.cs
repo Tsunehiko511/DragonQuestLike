6a792304d2d905a19ac64605caecffc757f8cdc5
嶋津恒彦 <simanezumi1989@gmail.com>
2021-01-06 00:41:30 +0900

    タイトルシーンの作成

---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] string nextScene;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
